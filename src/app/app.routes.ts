87693bf8387a14d2ad14fd9cc4c0852356779716
Menna-Ashraf512 <mennaalla@gmail.com>
2025-06-17 05:18:20 +0300

    feat: add authentication guard and protect routes; update login component styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guard/auth.guard';
 
 export const routes: Routes = [
     {path:'',redirectTo:'auth',pathMatch:'full'},
       {path:'auth',loadChildren:()=>import('./auth/auth.module').then(m=>m.AuthModule) },
-      {path:'home',loadComponent:()=>import('./home/home.component').then(m=>m.HomeComponent) },
-      {path:'cart',loadComponent:()=>import('./cart/cart.component').then(m=>m.CartComponent) },
-      {path:'allProduct',loadComponent:()=>import('./all-product/all-product.component').then(m=>m.AllProductComponent) },
+      {path:'home',loadComponent:()=>import('./home/home.component').then(m=>m.HomeComponent) ,canActivate:[authGuard]},
+      {path:'cart',loadComponent:()=>import('./cart/cart.component').then(m=>m.CartComponent) ,canActivate:[authGuard]},
+      {path:'allProduct',loadComponent:()=>import('./all-product/all-product.component').then(m=>m.AllProductComponent),canActivate:[authGuard] },
 
 ];
